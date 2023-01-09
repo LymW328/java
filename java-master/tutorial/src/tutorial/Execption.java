219f2e0425877aca657289384f84fae07be3a81d
diddnjs328 <diddnjs328@naver.com>
2023-01-09 22:11:03 +0900

    1

---
+++ java-master/tutorial/src/tutorial/Execption.java
@@ -0,0 +1,14 @@
+public class Execption {
+    class A{
+        private  int[] arr = new int[3];
+        A() {
+            arr[0]=0;
+            arr[1]=10;
+            arr[2]=20;
+
+
+        }
+    }
+
+    System.out.println();
+}
